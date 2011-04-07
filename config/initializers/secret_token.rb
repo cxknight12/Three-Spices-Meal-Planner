6dbb924ffb5b713336fcc5e1a3bfd1ce5e94df1f
David Taylor <cxknight12@gmail.com>
2011-04-07 14:01:23 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThreeSpicesMealPlanner::Application.config.secret_token = '89220a1806bb69714036df3fdb84f41d5278d1b50f659c184d47c95abe3fa1e95e7d3b679ba1e3c7491f93e0455d12ad5c6aed1fe83e5db4332478dcbdc39708'
